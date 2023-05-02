680b211561cb60fdeb54b6ba416e1d0ed2ab8fab
geeverma1 <v.geet86@gmail.com>
2023-05-02 19:33:24 +0530

    FIx

--- hello.test.js
+++ hello.test.js
@@ -1,6 +1,6 @@
 const hello = require("./hello");
 describe("My hello", () => {
   test("works", () => {
-    expect(hello.hello()).toEqual("Hello World from changedSpain");
+    expect(hello.hello()).toEqual("Hello World");
   });
 });
